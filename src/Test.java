180b6e8ac5bf1df3df18d10cf2d2d9b692b30db3
zxc <1272811460@qq.com>
2020-08-14 18:27:00 +0800

    git测试02

--- src/Test.java
+++ src/Test.java
@@ -7,6 +7,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("git合并");
         System.out.println("git合并master并再次提交新的01");
+        System.out.println("git合并master并再次提交新的02");
     }
 
 }
